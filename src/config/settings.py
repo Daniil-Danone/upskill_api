dd3fed6360f6d70e42b5d529f1fd0c446cc472a0
Daniil-Danone <mr.super.ko@inbox.ru>
2024-10-09 16:26:20 +0600

    chore: fix cors

--- src/config/settings.py
+++ src/config/settings.py
@@ -22,7 +22,9 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = '*'
+CORS_ALLOW_METHODS = '*'
 
 # Application definition
 INSTALLED_APPS = [
